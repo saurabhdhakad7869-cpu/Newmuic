efa686ec01e0e2df9d5df68d7b6493cb2bb56055
Saurabh dhakad <saurabhdhakad7869@gmail.com>
2026-03-26 09:17:39 +0530

    Update __init__.py

--- AnieXEricaMusic/__init__.py
+++ AnieXEricaMusic/__init__.py
@@ -1,3 +1,11 @@
+import asyncio
+
+# Event loop fix for Railway/Python 3.10+
+try:
+    asyncio.get_event_loop()
+except RuntimeError:
+    asyncio.set_event_loop(asyncio.new_event_loop())
+
 from AnieXEricaMusic.core.bot import AMBOT
 from AnieXEricaMusic.core.dir import dirr
 from AnieXEricaMusic.core.git import git
@@ -13,7 +21,6 @@ heroku()
 
 app = AMBOT()
 userbot = Userbot()
-
 
 from .platforms import *
 
